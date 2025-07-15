0bc33339a0dfc60809013d38cd7ac91d108d1e72
harshalBhawsar30 <harshal.b@ccube.com>
2025-07-15 16:45:12 +0530

    feat(api): handle empty string case12

--- .versionrc.js
+++ .versionrc.js
@@ -1,13 +1,4 @@
 module.exports = {
-  // Controls version bumps
-  releaseRules: [
-    { type: 'feat', scope: 'api', release: 'major' }, 
-    { type: 'feat', release: 'minor' },
-    { type: 'fix', release: 'patch' },
-    { breaking: true, release: 'major' }
-  ],
-
-  // Controls changelog formatting
   types: [
     { type: 'feat', section: 'Features' },
     { type: 'fix', section: 'Bug Fixes' },
