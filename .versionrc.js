510edb2f4e0ace5e1d2a9bf137c105666802ff4e
harshalBhawsar30 <harshal.b@ccube.com>
2025-07-15 16:38:02 +0530

    feat(api): log a new line for testing1

--- .versionrc.js
+++ .versionrc.js
@@ -1,9 +1,16 @@
 module.exports = {
-  types: [
-    { type: 'feat', scope: 'api', release: 'major' },
+  // Controls version bumps
+  releaseRules: [
+    { type: 'feat', scope: 'api', release: 'major' }, 
     { type: 'feat', release: 'minor' },
     { type: 'fix', release: 'patch' },
-    { breaking: true, release: 'major' },
+    { breaking: true, release: 'major' }
+  ],
+
+  // Controls changelog formatting
+  types: [
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
     { type: 'chore', section: 'Chores', hidden: false },
     { type: 'docs', section: 'Docs', hidden: false },
     { type: 'test', section: 'Tests', hidden: false },
